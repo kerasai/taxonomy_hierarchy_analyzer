325f112e26b6def4c1e0fdf010cbc5b95f86dedc
Will Long <will@kerasai.com>
2025-12-24 08:19:14 -0600

    method for find entity ref fields targeting a specified bundle

--- src/TaxonomyHierarchyAnalyzer.php
+++ src/TaxonomyHierarchyAnalyzer.php
@@ -136,4 +136,60 @@ class TaxonomyHierarchyAnalyzer {
     return $this->database->query($query, [':tid' => $tid])->fetchAll();
   }
 
+  /**
+   * Discover entity reference fields targeting a vocabulary.
+   *
+   * @param string $vid
+   *   Vocabulary ID.
+   *
+   * @return array
+   *   Array of field info with keys: entity_type, field_name, table, column.
+   */
+  public function getTaxonomyReferenceFields(string $vid): array {
+    $fields = [];
+    $field_map = $this->entityFieldManager->getFieldMapByFieldType('entity_reference');
+
+    foreach ($field_map as $entity_type_id => $entity_field_info) {
+      foreach ($entity_field_info as $field_name => $field_info) {
+        $table = "{$entity_type_id}__{$field_name}";
+
+        // Short circuit if we've already determined that we have this table.
+        if (!empty($fields[$table])) {
+          continue;
+        }
+
+        foreach ($field_info['bundles'] as $bundle) {
+          $definition = $this->entityFieldManager
+            ->getFieldDefinitions($entity_type_id, $bundle)[$field_name] ?? NULL;
+
+          if (!$definition) {
+            continue;
+          }
+
+          $settings = $definition->getSettings();
+
+          if (($settings['target_type'] ?? '') !== 'taxonomy_term') {
+            continue;
+          }
+
+          // Exclude if we're configured to target specific bundles, but not
+          // our bundle (vocabulary).
+          $target_bundles = $settings['handler_settings']['target_bundles'] ?? [];
+          if ($target_bundles && !in_array($bundle, $target_bundles)) {
+            continue;
+          }
+
+          $fields[$table] = [
+            'entity_type' => $entity_type_id,
+            'field_name' => $field_name,
+            'table' => $table,
+            'column' => "{$field_name}_target_id",
+          ];
+        }
+      }
+    }
+
+    return $fields;
+  }
+
 }
